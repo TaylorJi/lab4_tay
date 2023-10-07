1b6b78d6783e3e0a088c9fdc9ff9dcae22df224f
TaylorJi <sji24@my.bcit.ca>
2023-10-07 16:04:55 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+let dictionary = [];
+let counter = 0;
+
+http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const headers = {
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*',  // to allow cross-origin requests
+        'Access-Control-Allow-Methods': 'GET, POST'
+    };
+
+    if (q.pathname === '/search') {
+        fs.readFile(path.join(__dirname, 'search.html'), (err, data) => {
+            if (err) {
+                res.writeHead(404, headers);
+                res.end(JSON.stringify({ error: "Not Found Search" }));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+        return // return to prevent the rest of the code from running
+    }
+
+    if (q.pathname === '/store') {
+        fs.readFile(path.join(__dirname, 'store.html'), (err, data) => {
+            if (err) {
+                res.writeHead(404, headers);
+                res.end(JSON.stringify({ error: "Not Found Search" }));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+        return // return to prevent the rest of the code from running
+    }
+
+
+    if (q.pathname === "/api/definitions/" && req.method === "POST") {
+        let body = "";
+        req.on('data', chunk => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const { word, definition } = JSON.parse(body);
+            if (!word || !definition || !/^[a-zA-Z\s]+$/.test(word)) { // check if word and definition are valid, and if word contains only letters and spaces
+                res.writeHead(400, headers);
+                res.end(JSON.stringify({ error: "Invalid word or definition provided." }));
+                return;
+            }
+            // check if word already exists in the dictionary
+            const existing = dictionary.find(item => item.word.toLowerCase() === word.toLowerCase());
+            if (existing) {
+                res.writeHead(400, headers);
+                res.end(JSON.stringify({ error: `The word '${word}' already exists in the dictionary.` }));
+                return;
+            }
+            // if word is valid and doesn't exist, add it to the dictionary
+            dictionary.push({ word, definition });
+            counter++;
+            res.writeHead(200, headers);
+            res.end(JSON.stringify({
+                message: `Request #${counter}\nNew entry recorded:\n${word} : ${definition}`
+            }));
+        });
+    } else if (q.pathname === "/api/definitions/" && req.method === "GET") {
+        const { word } = q.query; // get the word from the query string
+        if (!word || !/^[a-zA-Z\s]+$/.test(word)) { // check if word is valid
+            res.writeHead(400, headers);
+            res.end(JSON.stringify({ error: "Invalid word provided." }));
+            return;
+        }
+        const entry = dictionary.find(item => item.word.toLowerCase() === word.toLowerCase()); // case insensitive, make them all lowercase
+        counter++;
+        // if word is not found, return 404, otherwise return the word and definition
+        if (!entry) {
+            res.writeHead(404, headers);
+            res.end(JSON.stringify({ message: `Request#${counter}, word '${word}' not found!` }));
+            return;
+        }
+        res.writeHead(200, headers);
+        res.end(JSON.stringify({ word: entry.word, definition: entry.definition }));
+    } else {
+        res.writeHead(404, headers);
+        res.end(JSON.stringify({ error: "Not Found" }));
+    }
+}).listen(8081, () => {
+    console.log('Backend server is running on http://localhost:8081/');
+});
